27576823c93fcc1f2985aa2c966d0216f91d71a6
msnaprl <mesniaprilia08@gmail.com>
2025-06-14 20:01:54 +0700

    01_visualisasi_data.py

--- pages/01_visualisasi_data.py
+++ pages/01_visualisasi_data.py
@@ -3,14 +3,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv("dataset.csv")
+if 'Nomor' in df.columns:
+    df = df.drop(columns=['Nomor'])
 
 st.title("📊 Visualisasi Data")
 
 st.dataframe(df)
 
-cols = df.columns.tolist()
-for col in cols:
+for col in df.columns:
     st.subheader(f"Distribusi {col}")
     fig, ax = plt.subplots()
     df[col].value_counts().plot(kind='bar', ax=ax)
     st.pyplot(fig)
+
